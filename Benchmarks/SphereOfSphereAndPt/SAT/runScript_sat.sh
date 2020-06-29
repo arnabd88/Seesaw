2d985761b2c5dc5bf7f7f59e19857127441b265d
arnab <arnab.d.88@gmail.com>
2020-06-29 03:02:47 -0600

    commit

--- Benchmarks/SphereOfSphereAndPt/SAT/runScript_sat.sh
+++ Benchmarks/SphereOfSphereAndPt/SAT/runScript_sat.sh
@@ -47,9 +47,9 @@ python3 ../../../src/satire+.py  \
         --report-instability \
 		--file ${testname} \
 		--stat-err-enable \
-		--stat-err-fraction 0.5 \
-		--logfile basic_instab_stat_0.5.log \
-		--outfile basic_instab_stat_0.5.out
+		--stat-err-fraction 0.8 \
+		--logfile basic_instab_stat_0.8.log \
+		--outfile basic_instab_stat_0.8.out
 
 #6. Config : Basic Solver + realpaver + statistical error with fractional threshold
 python3 ../../../src/satire+.py  \
@@ -59,9 +59,9 @@ python3 ../../../src/satire+.py  \
 		--file ${testname} \
 		--realpaver \
 		--stat-err-enable \
-		--stat-err-fraction 0.5 \
-		--logfile basic_instab_RP_stat_0.5.log \
-		--outfile basic_instab_RP_stat_0.5.out
+		--stat-err-fraction 0.8 \
+		--logfile basic_instab_RP_stat_0.8.log \
+		--outfile basic_instab_RP_stat_0.8.out
 
 ## ---------------- Constrained optimizer enabled -------------------------
 
--- Benchmarks/SqDistPointSegment/SAT/runScript_sat.sh
+++ Benchmarks/SqDistPointSegment/SAT/runScript_sat.sh
@@ -42,9 +42,9 @@ python3 ../../../src/satire+.py  \
         --report-instability \
 		--file ${testname} \
 		--stat-err-enable \
-		--stat-err-fraction 0.5 \
-		--logfile basic_instab_stat_0.5.log \
-		--outfile basic_instab_stat_0.5.out
+		--stat-err-fraction 0.8 \
+		--logfile basic_instab_stat_0.8.log \
+		--outfile basic_instab_stat_0.8.out
 
 #6. Config : Basic Solver + realpaver + statistical error with fractional threshold
 python3 ../../../src/satire+.py  \
