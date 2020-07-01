ed9e67cfe70c469fb413586a44e5766d48ff65cc
arnab <arnab.d.88@gmail.com>
2020-07-01 16:59:53 -0600

    run stats

---
+++ Benchmarks/ray_tracing/SAT/runScript_statistics.sh
@@ -0,0 +1,25 @@
+testname="ray_tracing.txt"
+
+
+for frac in 0.0 0.5 0.8 
+do
+
+	python3 ../../../src/satire+.py \
+			--std \
+			--file ${testname} \
+			--stat-err-enable \
+			--stat-err-fraction ${frac} \
+			--samples 10000 \
+			--logfile stat_basic_F${frac}.log \
+			--outfile stat_basic_F${frac}.out 
+
+	python3 ../../../src/satire+.py \
+			--std \
+			--file ${testname} \
+			--realpaver \
+			--stat-err-enable \
+			--stat-err-fraction ${frac} \
+			--samples 10000 \
+			--logfile stat_basic_RP_F${frac}.log \
+			--outfile stat_basic_RP_F${frac}.out 
+done
---
+++ Benchmarks/runScript_statistics.sh
@@ -0,0 +1,28 @@
+
+
+DIRS="barycentric_coordinates\
+	 ClosestPtPointSegment2\
+     EigenSphere\
+	 ExtremePointsAlongDirection\
+	 gram-schmidt\
+	 MD\
+	 smartRoot\
+	 SphereOfSphereAndPt\
+	 SqDistPointSegment\
+	 squareRoot3\
+	 styblinski\
+	 test2\
+	 SymSchur\
+	 Jacobi\
+	 lead-lag"
+
+
+set -x
+
+for d in $DIRS
+do
+	cd $d/SAT
+	echo $PWD
+	bash runScript_statistics.sh
+	cd ../../
+done
