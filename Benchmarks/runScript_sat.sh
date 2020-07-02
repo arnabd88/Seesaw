b77ebf2f5966ad9cf5dc5a3e358c6a94f3a6ea41
arnab <arnab.d.88@gmail.com>
2020-07-02 12:24:33 -0600

    removing Constrained+RP runs

--- Benchmarks/runScript_sat.sh
+++ Benchmarks/runScript_sat.sh
@@ -19,10 +19,18 @@ DIRS="barycentric_coordinates\
 
 set -x
 
+SAT_STAT_FILE="sat_stat.dat"
+
+if [[ -f $SAT_STAT_FILE ]]; then
+	rm -rf $SAT_STAT_FILE
+fi
+
 for d in $DIRS
 do
 	cd $d/SAT
-	echo $PWD
+	echo "Begining test $d" >> $SAT_STAT_FILE
+	echo $PWD >> $SAT_STAT_FILE
 	bash runScript_sat.sh
+	echo "Finished test $d \n\n" >> $SAT_STAT_FILE
 	cd ../../
 done
--- Benchmarks/runScript_statistics.sh
+++ Benchmarks/runScript_statistics.sh
@@ -19,10 +19,21 @@ DIRS="barycentric_coordinates\
 
 set -x
 
+SAT_STAT_FILE="sat_stat.dat"
+
+
+if [[ -f $SAT_STAT_FILE ]]; then
+	rm -rf $SAT_STAT_FILE
+fi
+
+
 for d in $DIRS
 do
 	cd $d/SAT
+	echo "Begining test $d" >> $SAT_STAT_FILE
+	echo $PWD >> $SAT_STAT_FILE
 	echo $PWD
 	bash runScript_statistics.sh
+	echo "Finished test $d \n\n" >> $SAT_STAT_FILE
 	cd ../../
 done
