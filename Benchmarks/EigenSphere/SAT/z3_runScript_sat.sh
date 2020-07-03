64e879d7f616e95a1f0edb10f1ce50ac0f05e4ed
arnab <arnab.d.88@gmail.com>
2020-07-03 10:51:15 -0600

    separating z3 runs

---
+++ Benchmarks/EigenSphere/SAT/z3_runScript_sat.sh
@@ -0,0 +1,28 @@
+
+testname="EigenSphere.txt"
+
+##---------------------------------------------------------
+##
+## ---------  Constrained solver with Z3 ------------
+##
+##---------------------------------------------------------
+# Confiig : Constrained Solver + instability
+python3 ../../../src/satire+.py  \
+        --std \
+		--file ${testname} \
+		--enable-constr \
+		--useZ3 \
+		--logfile Z3_Constrained.log \
+		--outfile Z3_Constrained.out
+
+# Confiig : Constrained Solver + instability
+python3 ../../../src/satire+.py  \
+        --std \
+        --report-instability \
+		--file ${testname} \
+		--enable-constr \
+		--useZ3 \
+		--logfile Z3_Constrained_instab.log \
+		--outfile Z3_Constrained_instab.out
+
+
---
+++ Benchmarks/z3_runScript_sat.sh
@@ -0,0 +1,36 @@
+
+
+DIRS="barycentric_coordinates\
+	 SymSchur\
+	 SqDistPointSegment\
+	 squareRoot3\
+	 styblinski\
+	 smartRoot\
+	 test2\
+	 lead-lag\
+	 gram-schmidt\
+	 ClosestPtPointSegment2\
+	 ExtremePointsAlongDirection\
+	 SphereOfSphereAndPt\
+	 Jacobi\
+     EigenSphere\
+	 MD"
+
+
+set -x
+
+SAT_STAT_FILE="z3_sat_stat.dat"
+
+if [[ -f $SAT_STAT_FILE ]]; then
+	rm -rf $SAT_STAT_FILE
+fi
+
+for d in $DIRS
+do
+	cd $d/SAT
+	echo "Begining test $d" >> $SAT_STAT_FILE
+	echo $PWD >> $SAT_STAT_FILE
+	bash runScript_sat.sh
+	echo "Finished test $d \n\n" >> $SAT_STAT_FILE
+	cd ../../
+done
