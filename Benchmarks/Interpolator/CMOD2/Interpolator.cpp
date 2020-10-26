470befd51b02f410614c3b268cffdec572b10d78
tanmaytirpankar <tirpankartanmay@gmail.com>
2020-10-26 17:50:35 -0600

    Adding CMOD2 test for Interpolator benchmark

---
+++ Benchmarks/Interpolator/CMOD2/Interpolator.cpp
@@ -0,0 +1,139 @@
+
+
+#include <cstdio>
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+#include<cmath>
+#include <quadmath.h>
+#include <time.h>
+#include <fstream>
+#include <sstream>
+
+#define _low  0.0
+#define _high 100.0
+
+
+using namespace std ;
+
+double _r_E ;
+
+template<class T>
+void init() {
+	_r_E = _low + static_cast<T> (rand())/(static_cast<T>(RAND_MAX/(_high - _low))) ;
+}
+
+template<class T>
+T execute_spec_precision(int conditions[])
+{
+
+	T r_E = _r_E ;
+	T res ;
+
+	if ( r_E < ((-703687441776639.0) / 140737488355328.0)) {
+	    conditions[0] = 1;
+		res = r_E * (9.0/4.0) ;
+	} else {
+	    conditions[0] = 0;
+		if (r_E >= (703687441776641.0 / 140737488355328.0)) {
+		    conditions[1] = 1;
+			if (r_E < (3518437208883199.0 / 140737488355328.0)) {
+			    conditions[2] = 1;
+				res = (((r_E - 5.0) * (2476979795053773.0 / 2251799813685248.0)) + (5.0 * (9.0 / 4.0))) ;
+			} else {
+			    conditions[2] = 0;
+				if (r_E >= (3518437208883201 / 140737488355328)) {
+				    conditions[3] = 1;
+					res = ((5.0 * (9.0 / 4.0)) + (20.0 * (2476979795053773.0 / 2251799813685248.0))) ;
+				} else {
+				    conditions[3] = 0;
+					res = 0 ;
+				}
+			}
+		} else {
+		    conditions[1] = 0;
+			res = 0 ;
+		}
+	}
+
+	return res ;
+}
+
+template <typename T>
+std::string to_string_with_precision(const T a_value, const int n = 6)
+{
+    std::ostringstream out;
+    out.precision(n);
+    out << std::fixed << a_value;
+    return out.str();
+}
+
+int main(int argc, char** argv)
+
+{
+
+	srand(time(0));
+
+	FILE *fp ;
+	int N;
+	sscanf(argv[1], "%d", &N);
+	fp = fopen("interpolator_profile.csv", "w+");
+    ofstream fp_divergence_inputs;
+	fp_divergence_inputs.open("interpolator_divergence_inputs.csv", ios::out | ios::app);
+
+    __float80 val_lp = 0;
+	__float80 val_dp = 0;
+	__float80 val_qp = 0;
+	__float80 err_qp_lp = 0;
+	__float80 err_qp_dp = 0;
+
+    __float80 maxerrlp = 0.0;
+	__float80 maxerrdp = 0.0 ;
+
+    int num_predicates = 4;
+	int conditions_lp[num_predicates];
+	int conditions_dp[num_predicates];
+	int conditions_qp[num_predicates];
+
+	for(int i=0; i<N; i++) {
+        for(int j = 0; j < num_predicates; j++) {
+            conditions_lp[j] = -1;
+            conditions_dp[j] = -1;
+            conditions_qp[j] = -1;
+        }
+		init<double>();
+		__float80 val_lp = (__float80) execute_spec_precision<float>(conditions_lp);
+		__float80 val_dp = (__float80) execute_spec_precision<double>(conditions_dp);
+		__float80 val_qp = (__float80) execute_spec_precision<__float128>(conditions_qp);
+
+        err_qp_lp += fabs(val_dp - val_lp);
+		err_qp_dp += fabs(val_qp - val_dp);
+
+        if ( maxerrlp < fabs(val_dp - val_lp)) maxerrlp = fabs(val_dp - val_lp) ;
+		if ( maxerrdp < fabs(val_qp - val_dp)) maxerrdp = fabs(val_qp - val_dp) ;
+		for(int j = 0; j < num_predicates; j++) {
+            if(conditions_lp[j] != conditions_dp[j] && conditions_lp[j] != -1 && conditions_dp[j] != -1) {
+                string str = "instability_lp:" + to_string_with_precision(fabs(val_dp - val_lp), 16) + ",Pred:" + to_string(j) + ",_r_E:" + to_string_with_precision(_r_E, 16) + "\n";
+                fp_divergence_inputs << str;
+                cout << str;
+            }
+            if(conditions_dp[j] != conditions_qp[j] && conditions_dp[j] != -1 && conditions_qp[j] != -1) {
+                string str = "instability_dp:" + to_string_with_precision(fabs(val_qp - val_dp), 16) + ",Pred:" + to_string(j) + ",_r_E:" + to_string_with_precision(_r_E, 16) + "\n";
+                fp_divergence_inputs << str;
+                cout << str;
+            }
+        }
+	}
+
+	fclose(fp);
+	fp_divergence_inputs.close();
+
+    cout << "Avg Error in LP -> " << err_qp_lp/N << endl ;
+	cout << "Max Error in LP -> " << maxerrlp << endl ;
+	cout << "Avg Error in DP -> " << err_qp_dp/N << endl ;
+	cout << "Max Error in DP -> " << maxerrdp << endl ;
+
+
+	return 1;
+
+}
