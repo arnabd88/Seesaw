336c4c438fa55a206f78cec32e9a6f6cbe1a98bd
tanmaytirpankar <tirpankartanmay@gmail.com>
2021-09-13 16:29:31 -0600

    Modified API to include CORS policy

--- src/apis/seesaw_api.py
+++ src/apis/seesaw_api.py
@@ -1,7 +1,8 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
+from flask_cors import CORS
 import sys
 import subprocess
 
@@ -35,64 +36,68 @@ def create_command(args):
         cmd.append('--enable-constr')
     return cmd
 
+@app.route("/seesaw", methods=["GET"])
+def get():
+    parser = reqparse.RequestParser()  # initialize
+    parser.add_argument('program')
+    parser.add_argument('abstraction')  # add args
+    parser.add_argument('abstraction_lower_bound')
+    parser.add_argument('abstraction_upper_bound')
+    parser.add_argument('parallel')
+    parser.add_argument('report_instability')
+    parser.add_argument('enable_constraints')
+    args = parser.parse_args()  # parse arguments to dictionary
 
-class Users(Resource):
-    def get(self):
-        parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('program')
-        parser.add_argument('abstraction')  # add args
-        parser.add_argument('abstraction_lower_bound')
-        parser.add_argument('abstraction_upper_bound')
-        parser.add_argument('parallel')
-        parser.add_argument('report_instability')
-        parser.add_argument('enable_constraints')
-        args = parser.parse_args()  # parse arguments to dictionary
+    cmd = create_command(args)
+    if not os.path.isdir("web_interface_programs"):
+        os.system('mkdir web_interface_programs')
+    # subprocess.call(["mkdir", "web_interface_programs"])
+    subprocess.call(["echo", args['program']], stdout=open("web_interface_programs/ex1", "w"))
+    # subprocess.check_output(["cat", "web_interface_programs/ex1"]).decode(sys.stdout.encoding)
+    print(cmd)
+    result = subprocess.check_output(cmd).decode(sys.stdout.encoding)
 
-        cmd = create_command(args)
-        if not os.path.isdir("web_interface_programs"):
-            os.system('mkdir web_interface_programs')
-        # subprocess.call(["mkdir", "web_interface_programs"])
-        subprocess.call(["echo", args['program']], stdout=open("web_interface_programs/ex1", "w"))
-        # subprocess.check_output(["cat", "web_interface_programs/ex1"]).decode(sys.stdout.encoding)
-        print(cmd)
-        result = subprocess.check_output(cmd).decode(sys.stdout.encoding)
+    # response = jsonify(message="Simple server is running")
+    # response.headers.add("Access-Control-Allow-Origin", "*")
 
-        subprocess.call(["rm", "-rf", "web_interface_programs"])
-        subprocess.call(["rm", "-rf", "default.txt"])
-        subprocess.call(["rm", "-rf", "outfile.txt"])
-        print(result)
-        return {'data': result}, 200
+    subprocess.call(["rm", "-rf", "web_interface_programs"])
+    subprocess.call(["rm", "-rf", "default.txt"])
+    subprocess.call(["rm", "-rf", "outfile.txt"])
+    print(result)
 
-    def post(self):
-        pass
-        # parser = reqparse.RequestParser()  # initialize
-        # parser.add_argument('userId', required=True)  # add args
-        # parser.add_argument('name', required=True)
-        # parser.add_argument('city', required=True)
-        # args = parser.parse_args()  # parse arguments to dictionary
-        # 
-        # # read our CSV
-        # data = pd.read_csv('users.csv')
-        # 
-        # if args['userId'] in list(data['userId']):
-        #     return {
-        #         'message': f"'{args['userId']}' already exists."
-        #     }, 409
-        # else:
-        #     # create new dataframe containing new values
-        #     new_data = pd.DataFrame({
-        #         'userId': [args['userId']],
-        #         'name': [args['name']],
-        #         'city': [args['city']],
-        #         'locations': [[]]
-        #     })
-        #     # add the newly provided values
-        #     data = data.append(new_data, ignore_index=True)
-        #     data.to_csv('users.csv', index=False)  # save back to CSV
-        #     return {'data': data.to_dict()}, 200  # return data with 200 OK
+    # return response
+    return {'data': result}, 200
 
+@app.route("/seesaw", methods=["POST"])
+def post():
+    pass
+    # parser = reqparse.RequestParser()  # initialize
+    # parser.add_argument('userId', required=True)  # add args
+    # parser.add_argument('name', required=True)
+    # parser.add_argument('city', required=True)
+    # args = parser.parse_args()  # parse arguments to dictionary
+    #
+    # # read our CSV
+    # data = pd.read_csv('users.csv')
+    #
+    # if args['userId'] in list(data['userId']):
+    #     return {
+    #         'message': f"'{args['userId']}' already exists."
+    #     }, 409
+    # else:
+    #     # create new dataframe containing new values
+    #     new_data = pd.DataFrame({
+    #         'userId': [args['userId']],
+    #         'name': [args['name']],
+    #         'city': [args['city']],
+    #         'locations': [[]]
+    #     })
+    #     # add the newly provided values
+    #     data = data.append(new_data, ignore_index=True)
+    #     data.to_csv('users.csv', index=False)  # save back to CSV
+    #     return {'data': data.to_dict()}, 200  # return data with 200 OK
 
-api.add_resource(Users, '/users')  # add endpoints
+cors = CORS(app)
 
 if __name__ == '__main__':
     app.run()  # run our Flask app
